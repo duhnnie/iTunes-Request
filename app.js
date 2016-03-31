842c7a8c17f450a17ffe0ba76392282c58e67164
Daniel <dnl.canedo@gmail.com>
2016-03-31 12:47:20 -0400

    Remove unnnecessary files and mod index

--- app.js
+++
@@ -1,6 +0,0 @@
-var iTunesControl = require("itunescontrol");
-
-iTunesControl.search("Llegas aburrimiento", function(results) {
-	console.log("Results!", results);
-	iTunesControl.play(results[0].id);
-});
